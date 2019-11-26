8c2291a2abe3825386ff2a06317fcc1bd1fec991
Alexander Malyshev <san-100@bk.ru>
2019-11-26 23:36:53 +0300

    Добавлен один тест

--- app/src/androidTest/java/com/moonpi/swiftnotes/test/SimpleTest.kt
+++ app/src/androidTest/java/com/moonpi/swiftnotes/test/SimpleTest.kt
@@ -5,6 +5,7 @@ import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.runner.AndroidJUnit4
+import android.widget.ImageButton
 import com.moonpi.swiftnotes.MainActivity
 import com.moonpi.swiftnotes.R
 import com.moonpi.swiftnotes.rule.SwiftnotesRule
@@ -39,12 +40,21 @@ class SimpleTest : AbstractSwiftnotesTest() {
     @DisplayName("Проверка экрана создания заметки")
     fun noteCreationScreen() {
         rule.launchActivity()
-        step("Зайти в приложение") {
+        step("Зайти на форму создания заметки") {
             onView(withId(R.id.newNote)).perform(click())
             onView(allOf(withId(R.id.titleEdit), isDisplayed())).check(matches(withHint("Title")))
             onView(allOf(withId(R.id.bodyEdit), isDisplayed())).check(matches(withHint("Note")))
             onView(allOf(withId(R.id.toolbar), isDisplayed()))
-            onView(allOf(withId(R.drawable.abc_ic_ab_back_material), isDisplayed())).perform(click())
+        }
+        step("Выйти с формы создания заметки") {
+            onView(withClassName(containsString("ImageButton"))).perform(click())
+            onView(allOf(withClassName(containsString("FrameLayout")), isDisplayed()))
+            onView(allOf(withClassName(containsString("TextView")), isDisplayed())).check(matches(withText("Save changes?")))
+            onView(allOf(withId(android.R.id.button1), isDisplayed())).check(matches(withText("YES")))
+            onView(allOf(withId(android.R.id.button2), isDisplayed())).check(matches(withText("NO")))
+            onView(withId(android.R.id.button2)).perform(click())
+            onView(allOf(withId(R.id.toolbar), isDisplayed()))
+            onView(allOf(withId(R.id.toolbar), withChild(withText("Swiftnotes"))))
         }
     }
 }
